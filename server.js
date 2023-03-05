d0f0961c9ee5f42ff7c3aa1ad54eb50d1da3b213
Jeremy THEBAULT <j.thebault@techfirm.ch>
2023-03-05 06:37:35 +0100

    indentation

--- server.js
+++ server.js
@@ -15,11 +15,42 @@ app.use(cors());
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '.', 'index.html'));
+    res.sendFile(path.join(__dirname, '.', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
 
+
+// server-side Node.js code
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    if (req.method === 'POST' && req.url === '/api/write-score') {
+        console.log("write 1");
+        let data = '';
+        req.on('data', (chunk) => {
+            data += chunk;
+        });
+        req.on('end', () => {
+            const parsedData = JSON.parse(data);
+            fs.appendFile('scores.txt', parsedData.data, (err) => {
+                if (err) {
+                    res.statusCode = 500;
+                    res.end('Error writing file');
+                } else {
+                    res.statusCode = 200;
+                    res.end('File written successfully');
+                }
+            });
+        });
+    } else {
+        res.statusCode = 404;
+        res.end('Not found');
+    }
+}).listen(3000);
+
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
